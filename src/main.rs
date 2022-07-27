225383ece472addc8bde9716cf62e6f4fbf2d606
JackMBenson <n10255460@qut.edu.au>
2022-07-27 12:02:10 +1000

    got the uefi version to POST

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 #![no_std]
 #![no_main]
-#![reexport_test_harness_main = "test_main"]
-#![feature(custom_test_frameworks)]
-#![test_runner(crate::test::test_runner)]
 
 #[macro_use]
 extern crate lazy_static;
@@ -10,14 +7,8 @@ extern crate lazy_static;
 //use terminal::*;
 
 mod panic;
-mod qemu;
-mod terminal;
-mod test;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    #[cfg(test)]
-    test_main();
-
     loop {}
 }
